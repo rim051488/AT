a81f5ef224b359ea8f1f216c70aac1c0f6cdbc69
rim051488 <sin.naga71@outlook.jp>
2022-04-22 15:41:30 +0900

    a

--- GamePG/GamePG/main.cpp
+++ GamePG/GamePG/main.cpp
@@ -14,27 +14,27 @@ struct Test {
 // SJIS(マルチバイト文字)で得た文字をワイド文字(Unicode)に変換
 // str : マルチバイト文字
 // 戻り値 : ワイド文字
-//wstring ConvertToWstring(const std::string& str)
-//{
-//	int cnt = MultiByteToWideChar(// 1回目はいくついるのかがわからないから文字数を取得する
-//		CP_UTF8,
-//		0,
-//		str.c_str(),			// 元の文字
-//		str.length(),			// 元の文字数
-//		nullptr,				// 変換後のアドレス
-//		0);						// 変換後の文字数 (変換後の文字数がわからないので0とする)
-//	wstring wstr;				// ユニコードが入れられる(ワイド文字)
-//	wstr.resize(cnt);
-//	MultiByteToWideChar(		// 必要な文字数がわかったので変換する
-//		CP_UTF8,
-//		0,
-//		str.c_str(),
-//		str.length(),
-//		&wstr[0],				// 受け取るアドレス
-//		wstr.length()			// 受け取る文字数
-//	);
-//	return wstr;
-//}
+wstring ConvertToWstring(const std::string& str)
+{
+	int cnt = MultiByteToWideChar(// 1回目はいくついるのかがわからないから文字数を取得する
+		CP_UTF8,
+		0,
+		str.c_str(),			// 元の文字
+		str.length(),			// 元の文字数
+		nullptr,				// 変換後のアドレス
+		0);						// 変換後の文字数 (変換後の文字数がわからないので0とする)
+	wstring wstr;				// ユニコードが入れられる(ワイド文字)
+	wstr.resize(cnt);
+	MultiByteToWideChar(		// 必要な文字数がわかったので変換する
+		CP_UTF8,
+		0,
+		str.c_str(),
+		str.length(),
+		&wstr[0],				// 受け取るアドレス
+		wstr.length()			// 受け取る文字数
+	);
+	return wstr;
+}
 
 //int main() {
 //	FILE* fp = nullptr;			// *ポインター型ポインターのメモリしか確保されない実体なし
