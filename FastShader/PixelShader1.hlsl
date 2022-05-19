159b9a61b5c306909d5c4c4a663152e4ffba08a4
rim051488 <sin.naga71@outlook.jp>
2022-05-19 13:40:26 +0900

    z

--- FastShader/PixelShader1.hlsl
+++ FastShader/PixelShader1.hlsl
@@ -9,27 +9,12 @@ struct PS_INPUT {
 struct PS_OUTPUT {
 	float4 color				:SV_TARGET0;// 色
 };
-
-// 定数バッファ
-float threshold : register(c0);
-
-
 SamplerState sam;// サンプラ
-Texture2D tex:register(t0);
-Texture2D ptn:register(t1);
-//テクスチャ(最大８個までできる)
+Texture2D tex;//テクスチャ(最大８個までできる)
 // 1ピクセル１ピクセル塗りつぶし
 PS_OUTPUT main(PS_INPUT input) {
 	PS_OUTPUT output;
-	float discval = ptn.Sample(sam, input.uv).r;
-	if (discval > threshold) {
-		discard;
-	}
-	float4 col = tex.Sample(sam, input.uv);//float4(input.uv, 0, 1);// 赤を返す
-	output.color = col;
-	if (col.a == 0.0) {
-		discard;// なかったことにする
-	}
-	output.color.rgb = 1.0 - col.rgb;
+	output.color = tex.Sample(sam, input.uv);//float4(input.uv, 0, 1);// 赤を返す
+	output.color.rgb = 1.0 - output.color.rgb;
 	return output;
 }
--- FastShader/main.cpp
+++ FastShader/main.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-void MyDrawGraph(int x, int y, int imghandle,int pshandle) {
+void MyDrawGraph(int x, int y, int imghandle,int secondImg,int pshandle) {
 	int width, haight;
 	GetGraphSize(imghandle,&width,&haight);
 	array<VERTEX2DSHADER, 4> verts;
@@ -34,9 +34,12 @@ void MyDrawGraph(int x, int y, int imghandle,int pshandle) {
 	verts[3].u = 1.0f;
 	verts[3].v = 1.0f;
 	int alphamode, alphaparam;
+	SetDrawBlendMode(DX_BLENDGRAPHTYPE_ALPHA, 255);
 	GetDrawAlphaTest(&alphamode,&alphaparam);
+	//GetDrawAlphaTest(DX_CMP_GREATER, 0);
 	SetUsePixelShader(pshandle);
 	SetUseTextureToShader(0, imghandle);
+	SetUseTextureToShader(1, secondImg);
 	DrawPrimitive2DToShader(verts.data(), verts.size(), DX_PRIMTYPE_TRIANGLESTRIP);
 }
 
@@ -45,14 +48,33 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, 
 	ChangeWindowMode(true);
 	DxLib_Init();
 	SetDrawScreen(DX_SCREEN_BACK);
-	int img = LoadGraph(L"bg_takeyabu.jpg");
+	int img = LoadGraph(L"zinbutu.png");
+	int ptn = LoadGraph(L"ptn.png");
+	int bg = LoadGraph(L"huukei.png");
 	int pps = LoadPixelShader(L"PixelShader.pso");
 	//int ps = LoadPixelShader(L"test.pso");
+
+	// 定数バッファの確保
+	int cbuff = DxLib::CreateShaderConstantBuffer(sizeof(float) * 4);
+	float* threshold = static_cast<float*>(DxLib::GetBufferShaderConstantBuffer(cbuff));
+	float t = 0.5f;
+	char keystate[256];
 	while (ProcessMessage() != -1) {
+		GetHitKeyStateAll(keystate);
+		if (keystate[KEY_INPUT_UP]){
+			t -= 0.01f;
+		}
+		if (keystate[KEY_INPUT_DOWN]){
+			t += 0.01f;
+		}
 		ClearDrawScreen();
+		DrawGraph(70, 0, bg, true);
 		//DrawGraph(0, 0, img, true);
 		//MyDrawGraph(0, 0, img, ps);
-		MyDrawGraph(0, 0, img, pps);
+		threshold[0] = t;
+		UpdateShaderConstantBuffer(cbuff);
+		SetShaderConstantBuffer(cbuff, DX_SHADERTYPE_PIXEL, 0);
+		MyDrawGraph(20, 0, img,ptn ,pps);
 		ScreenFlip();
 	}
 	DxLib_End();
